95ceae2f6a25b6db9970be6563b2d4bc34799903
Julionml <julionml78@gmail.com>
2021-04-08 22:19:56 -0300

    write of test ready

--- js-numerals/testNumbersToWords.js
+++ js-numerals/testNumbersToWords.js
@@ -1,36 +1,46 @@
 
 
 function testConvert() {
-    let testResult = document.querySelector('#testResult');
-    if (doConvert(7).trim() == 'seven')
-        console.log('It Works!');
-    else
-        console.log('Test failed');
+    alert('open the console to see the results(Press F12)')
 
-    if (doConvert(42) == 'forty two  ')
-        console.log('It Works!');
-    else
-        console.log('Test failed');
+    if (doConvert(7).trim() == 'seven') {
+        console.log('Input: 7. result:', doConvert(7), 'It Works!');
+    }
 
-        if (doConvert(2001) == 'two  thousand one ')
-        console.log('It Works!');
-    else
-        console.log('Test failed');
+    else {
+        console.log('Input: 7, result:', doConvert(7), 'Test failed');
+    }
 
-        if (doConvert(1999) == 'one  thousand nine hundred ninety nine  '){
-            testResult.innerHTML += doConvert(17999)+' 17999'
-            console.log('It Works!');
-        }
-        
-    else
-        console.log('Test failed');
-        
-        if (doConvert(17999) == 'seventeen  thousand nine hundred ninety nine  '){
-            console.log('It Works!');
-            testResult.innerHTML = doConvert(17999)
-        }
-        
-    else
-        console.log('Test failed');
-       
+    if (doConvert(42) == 'forty two  ') {
+        console.log('Input: 42. result:', doConvert(42), 'It Works!');
+    }
+
+    else {
+        console.log('Input: 42, result:', doConvert(42), 'Test failed');
+    }
+
+    if (doConvert(2001) == 'two  thousand one ') {
+        console.log('Input: 2001. result:', doConvert(2001), 'It Works!');
+    }
+
+    else {
+        console.log('Input: 2001, result:', doConvert(2001), 'Test failed');
+    }
+
+    if (doConvert(1999) == 'one  thousand nine hundred ninety nine  ') {
+        console.log('Input: 1999. result:', doConvert(1999), 'It Works!');
+    }
+
+    else {
+        console.log('Input: 1999, result:', doConvert(1999), 'Test failed');
+    }
+
+    if (doConvert(17999) == 'seventeen  thousand nine hundred ninety nine  ') {
+        console.log('Input: 17999. result:', doConvert(17999), 'It Works!');
+    }
+
+    else {
+        console.log('Input: 17999, result:', doConvert(17999), 'Test failed');
+    }
+
 }
