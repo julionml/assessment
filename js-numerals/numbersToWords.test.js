22da0a868f4cb88cb38631f7b4a90f43040f93c9
Julionml <julionml78@gmail.com>
2021-04-09 18:01:50 -0300

    Crating a separe test for each case

--- js-numerals/numbersToWords.test.js
+++ js-numerals/numbersToWords.test.js
@@ -1,9 +1,26 @@
 const doConvert = require('./numbersToWords')
 
-test('convert num to letters',()=>{
-    expect(doConvert(7)).toBe('seven ')
-    expect(doConvert(42)).toBe('forty two  ')
-    expect(doConvert(2001)).toBe('two  thousand one ')
-    expect(doConvert(1999)).toBe('one  thousand nine hundred ninety nine  ')
-    expect(doConvert(17999)).toBe('seventeen  thousand nine hundred ninety nine  ')
+test('convert number 7 to letters',()=>{
+    expect(doConvert(7)).toBe('seven')
+    
+})
+
+test('convert number 42 to letters',()=>{
+    
+    expect(doConvert(42)).toBe('forty two')
+})
+
+test('convert number 2001 to letters',()=>{
+    
+    expect(doConvert(2001)).toBe('two  thousand one')   
+})
+
+test('convert number 1999 to letters',()=>{
+    
+    expect(doConvert(1999)).toBe('one  thousand nine hundred ninety nine')   
+})
+
+test('convert number 17999 to letters',()=>{
+    
+    expect(doConvert(17999)).toBe('seventeen  thousand nine hundred ninety nine')
 })
